963e25e99a97795e0e383d857156aaccefdb4b06
Christof <chrak@cmja.de>
2021-02-25 15:39:13 +0100

    Auslagerung

---
+++ rfCards.h
@@ -0,0 +1,13 @@
+#ifndef RFCARDS_H_INCLUDED
+#define RFCARDS_H_INCLUDED
+
+
+uint8_t get_card_number();
+uint8_t clear_card();
+uint8_t try_card(uint8_t cardnum);
+uint8_t make_new_card(uint8_t cardnum);
+uint8_t make_new_zero_card();
+uint8_t prog_card(uint8_t cardnum);
+uint8_t getAddress(char adr);
+
+#endif // RFCARDS_H_INCLUDED
