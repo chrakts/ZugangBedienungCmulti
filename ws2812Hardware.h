a408c3b55c4c72817425e7ded44001e1b78962bc
Christof <chrak@cmja.de>
2021-02-25 21:24:16 +0100

    ws2812-Bib

--- ws2812Hardware.h
+++ ws2812Hardware.h
@@ -37,6 +37,17 @@
 
 #elif defined WS_ARCH_XMEGA
 
+/*  Voraussetzung bisher:
+    Timer 0
+    Port und Timer hängen zusammen (Timer C0 -> PORTC)
+    Port C: PIN 0/1/2/3
+    Port D: PIN 0/1/2/3
+    Port E: PIN 0/1/2/3
+    Ports C/D können remapped werden so, dass Pin 0->4 / 1->5 / 2->6 / 3->7 wird
+    Achtung beim remapping von Pin 0 und 1: Der PWM-Ausgang wird verodert mit dem Timer 1
+    getestet: C0,C1,C2,C3,C4,!C5!,C6,C7
+              D0,!D1!,!D2!,!D3!,D4,!D5!,!D6!,!D7!
+*/
 #define     WS_BUFNUM       6              // Output bytes to buffer
 #define     WS_BUFSIZE      (WS_BUFNUM*8)   // required buffer size for this
 
@@ -46,15 +57,70 @@
 #define     WS_DMA_DBUFMODE_bm DMA_DBUFMODE0_bm
 #define     WS_DMA_BUSY_bm  (DMA_CH0BUSY_bm|DMA_CH1BUSY_bm)
 #define     WS_DMA_INTLVL   0x03        // Interruptlevel for DMA: Original war 0x02
-#define     WS_TCC          TCC0        // Timer/Counter to use
-
 #define     WS_DMA_TRIGSRC  0x01        // TRIGSRC = EVSYS CH0
 #define     WS_EVSYS_CH     0           // Eventchannel to use
-#define     WS_EVSYS_MUXVAL 0b11000000  // Event selection == TCC0OVF
 #define     WS_WGMODE       0x03        // WGMODE_SS
-#define     WS_OUTPORT      PORTC       // output port
-#define     WS_OUTPIN       4           // output pin
 
-#endif
+#define     WS_OUTPIN 4         // output pin
+#define     WS_C_PORT
+
+#if WS_OUTPIN == 0
+  #define CCx          CCA
+  #define CCxBUF       CCABUF
+  #define TC0_CCxEN_bm TC0_CCAEN_bm
+#elif WS_OUTPIN == 1
+  #define CCx    CCB
+  #define CCxBUF CCBBUF
+  #define TC0_CCxEN_bm TC0_CCBEN_bm
+#elif WS_OUTPIN == 2
+  #define CCx    CCC
+  #define CCxBUF CCCBUF
+  #define TC0_CCxEN_bm TC0_CCCEN_bm
+#elif WS_OUTPIN == 3
+  #define CCx    CCD
+  #define CCxBUF CCDBUF
+  #define TC0_CCxEN_bm TC0_CCDEN_bm
+#elif WS_OUTPIN == 4
+  #define CCx    CCA
+  #define CCxBUF CCABUF
+  #define TC0_CCxEN_bm TC0_CCAEN_bm
+  #define TCC_REMAPPING PORT_TC0A_bm
+#elif WS_OUTPIN == 5
+  #define CCx    CCB
+  #define CCxBUF CCBBUF
+  #define TC0_CCxEN_bm TC0_CCBEN_bm
+  #define TCC_REMAPPING PORT_TC0B_bm
+#elif WS_OUTPIN == 6
+  #define CCx    CCC
+  #define CCxBUF CCCBUF
+  #define TC0_CCxEN_bm TC0_CCCEN_bm
+  #define TCC_REMAPPING PORT_TC0C_bm
+#elif WS_OUTPIN == 7
+  #define CCx    CCD
+  #define CCxBUF CCDBUF
+  #define TC0_CCxEN_bm TC0_CCDEN_bm
+  #define TCC_REMAPPING PORT_TC0D_bm
+#else
+  #error "Ungültige Pinnummer für WS_OUTPIN"
+#endif // WS_OUTPIN
+
+
+#ifdef WS_C_PORT
+#define     WS_TCC          TCC0        // Timer/Counter to use
+#define     WS_OUTPORT      PORTC       // output port
+#define     WS_EVSYS_MUXVAL 0b11000000
+#endif // WS_OUTPORT
+#ifdef WS_D_PORT
+#define     WS_TCC          TCD0        // Timer/Counter to use
+#define     WS_OUTPORT      PORTD       // output port
+#define     WS_EVSYS_MUXVAL 0b11010000
+#endif // WS_OUTPORT
+#ifdef WS_E_PORT
+#define     WS_TCC          TCE0        // Timer/Counter to use
+#define     WS_OUTPORT      PORTE       // output port
+#define     WS_EVSYS_MUXVAL 0b11100000
+#endif // WS_OUTPORT
+
+#endif // WS_ARCH_XMEGA
 
 #endif // WS2812HARDWARE_H_INCLUDED
