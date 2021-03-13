b40500dc18abe1c3eea089e0c378c864b8af2c7b
Christof <chrak@cmja.de>
2021-03-13 19:28:53 +0100

    timer-Zeiten angepasst

--- myTimers.cpp
+++ myTimers.cpp
@@ -12,10 +12,10 @@ extern volatile uint8_t do_sleep;
 volatile TIMER MyTimers[MYTIMER_NUM]= {	{TM_STOP,RESTART_NO,100,0,Beeper_Ready},
   									{TM_START,RESTART_YES,1000,0,nextReport},
 										{TM_STOP,RESTART_NO,100,0,NULL},			// Eingabe abgelaufen
-										{TM_START,RESTART_NO,250,0,LED_toggle},
-										{TM_STOP,RESTART_NO,500,0,Licht_Gross},
-										{TM_STOP,RESTART_NO,250,0,resetCardStatus},	// Blockade nach falscher Eingabe
-										{TM_START,RESTART_YES,1,0,Klingel_LED_PWM},	// Blockade nach falscher Eingabe
+										{TM_START,RESTART_NO,6,0,startKlingel},
+										{TM_STOP,RESTART_NO,300,0,Licht_Gross},
+										{TM_STOP,RESTART_NO,250,0,resetCardStatus},	// Timeout für Karte
+										{TM_STOP,RESTART_YES,1,0,Klingel_LED_PWM},	// Blockade nach falscher Eingabe
                     {TM_START,RESTART_YES,1,0,makeAdcMeasure}
 };
 
@@ -48,24 +48,27 @@ void Beeper_Ready(uint8_t test)
 	BEEPER_OFF;
 }
 
-void LED_toggle(uint8_t test)
+void startKlingel(uint8_t test)
 {
-//	LED_KLINGEL_TOGGLE;
+	if( (PORTB_IN & KLINGEL_TASTER_PIN)==0 )
+	{
+    Klingel_Trigger = 1;
+	}
 }
 
 void Licht_Gross(uint8_t test)
 {
 	STROM_STOSS_OFF;
-	set_led_color(F_GRUEN,BR_NOCHANGE,LED_LICHT_GROSS);
+	LED_ROT_OFF;
 }
 
 
 void resetCardStatus(uint8_t test)
 {
   MyTimers[TIMER_CARDSTATUS].state = TM_STOP;
-	cardStatus = false;
+	cardStatus = CARD_UNSELECTED;
   MFRC522_Init();
-  _delay_ms(2);
+  //_delay_ms(2);
 }
 
 void Klingel_LED_PWM(uint8_t test)
--- myTimers.h
+++ myTimers.h
@@ -15,12 +15,12 @@
 #define MYTIMER_NUM	8
 
 
-enum{TIMER_BEEPER_OFF,TIMER_NEXT_REPORT,TIMER_TIMEOUT,TIMER_RECALL,TIMER_LICHT_GROSS,TIMER_CARDSTATUS,TIMER_LED_KLINGEL,TIMER_ADC};
+enum{TIMER_BEEPER_OFF,TIMER_NEXT_REPORT,TIMER_TIMEOUT,TIMER_START_KLINGEL,TIMER_LICHT_GROSS,TIMER_CARDSTATUS,TIMER_LED_KLINGEL,TIMER_ADC};
 
 void nextReport(uint8_t test);
 void makeAdcMeasure(uint8_t test);
 void Beeper_Ready(uint8_t test);
-void LED_toggle(uint8_t test);
+void startKlingel(uint8_t test);
 void Licht_Gross(uint8_t test);
 void goto_sleep(uint8_t test);
 void resetCardStatus(uint8_t test);
